45fae5fe57adb5237d077942ea34d9d22696fdd9
Cosm3No1de <cosmenoide@gmail.com>
2025-05-08 17:07:04 -0400

    Primer commit: portafolio angular

---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true, // 👈 Esto es clave
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+
+}
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true, // 👈 Esto es clave
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+}
